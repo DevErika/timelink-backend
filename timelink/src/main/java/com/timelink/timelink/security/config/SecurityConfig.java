614476aff913138403fc5341a50ca313394bb754
DevErika <erikamontoya2023@gmail.com>
2025-11-19 16:30:47 +0100

    feat: add CORS config and clean SecurityConfig

--- timelink/src/main/java/com/timelink/timelink/security/config/SecurityConfig.java
+++ timelink/src/main/java/com/timelink/timelink/security/config/SecurityConfig.java
@@ -11,22 +11,43 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 import org.springframework.security.web.SecurityFilterChain;
 
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    // CORS CONFIGURATION
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5173") // FE con Vite
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+
+    // 2. SPRING SECURITY
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
-            .csrf(csrf -> csrf.disable()) // desactivado CSRF para usar Postman
+            .csrf(csrf -> csrf.disable()) // necesario para React
+            .cors(cors -> {}) // <-- ACTIVAR CORS EN SPRING SECURITY
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/api/users/**").permitAll() 
-                // aquí se protegerán  endpoints
+                .requestMatchers("/api/users/**").permitAll()
                 .anyRequest().authenticated()
             )
-            .formLogin(login -> login.disable())  // desactiva formulario login de Spring
-            .httpBasic(basic -> basic.disable()); // desactiva auth básica
+            .formLogin(login -> login.disable())
+            .httpBasic(basic -> basic.disable());
 
         return http.build();
     }
